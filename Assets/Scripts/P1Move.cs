4f6f844d86bca4ad38f0128c625972083cbad7a1
Samuel Cox/sunsetworld <68568350+sunsetworld@users.noreply.github.com>
2022-06-07 09:27:00 +0100

    End of section 3.

--- Assets/Scripts/P1Move.cs
+++ Assets/Scripts/P1Move.cs
@@ -5,15 +5,67 @@ using UnityEngine;
 public class P1Move : MonoBehaviour
 {
     public Rigidbody rb;
+    public float ForwardForce = 4000;
+    public float SidewaysForce = 500;
+
+    bool MoveLeft = false;
+    bool MoveRight = false;
+    bool MoveForward = false;
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("こんにちは、せかい！");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        rb.AddForce(0, 0, 4000 * Time.deltaTime);
+        if (Input.GetKey("w"))
+        {
+            MoveForward = true;
+        }
+        else
+        {
+            MoveForward = false;
+        }
+
+        if (Input.GetKey("d"))
+        {
+            MoveRight = true;
+        }
+        else
+        {
+            MoveRight= false;
+        }
+
+        if (Input.GetKey("a"))
+        {
+            MoveLeft = true;
+        }
+        else
+        {
+            MoveLeft = false;
+        }
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, ForwardForce / 2 * Time.deltaTime); // Moves player forward at half speed.
+
+        if (MoveForward)
+        {
+            rb.AddForce(0, 0, ForwardForce * Time.deltaTime); // Moves player forward at full speed.
+        }
+
+        if (MoveRight)
+        {
+            rb.AddForce(SidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if (MoveLeft)
+        {
+            rb.AddForce(-SidewaysForce * Time.deltaTime, 0, 0);
+        }
+
     }
 }
